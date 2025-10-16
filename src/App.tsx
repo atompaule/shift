0641577f6d7095dbaf70318885a492c406f38276
paulutsch <unoutsch@gmail.com>
2025-10-16 18:14:00 +0200

    Import sorting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react' 
-import './App.css'
+import "./App.css"
+
 import { Button } from "@/components/ui/button"
-
-
 import { Textarea } from "@/components/ui/textarea"
 
-
-function App() { 
-
+function App() {
   return (
-    <> 
-    <Textarea placeholder="Type your message here." />
-    <Button variant="outline">Button</Button>
-
+    <>
+      <Textarea placeholder="Type your message here." />
+      <Button variant="outline">Button</Button>
     </>
   )
 }
