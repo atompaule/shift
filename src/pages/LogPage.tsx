9bccfcbb6e5a0d8523b8971621240a46abf717b4
paulutsch <unoutsch@gmail.com>
2025-10-17 23:51:52 +0200

    Adds mock topic cloud component, includes it in LogPage

--- src/pages/LogPage.tsx
+++ src/pages/LogPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 
+import TopicCloud from "@/components/TopicCloud"
 import { Button } from "@/components/ui/button"
 import { ArrowUpIcon } from "@/components/ui/icons/lucide-arrow-up"
 import { Textarea } from "@/components/ui/textarea"
@@ -17,21 +18,28 @@ const LogPage = () => {
   }
 
   return (
-    <div className="flex flex-row justify-center absolute bottom-0 left-0 right-0">
-      <div className="flex flex-row gap-3 w-[600px] p-4">
-        <Textarea
-          placeholder="Type your message here."
-          onChange={onMessageChange}
-        />
-        <div className="flex flex-col justify-end">
-          <Button
-            variant="outline"
-            size="icon"
-            aria-label="Submit"
-            onClick={onSubmit}
-          >
-            <ArrowUpIcon />
-          </Button>
+    <div>
+      <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+        <div className="w-full h-[520px]">
+          <TopicCloud />
+        </div>
+      </div>
+      <div className="flex flex-row justify-center absolute bottom-0 left-0 right-0">
+        <div className="flex flex-row gap-3 w-[600px] p-4">
+          <Textarea
+            placeholder="Type your message here."
+            onChange={onMessageChange}
+          />
+          <div className="flex flex-col justify-end">
+            <Button
+              variant="outline"
+              size="icon"
+              aria-label="Submit"
+              onClick={onSubmit}
+            >
+              <ArrowUpIcon />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
